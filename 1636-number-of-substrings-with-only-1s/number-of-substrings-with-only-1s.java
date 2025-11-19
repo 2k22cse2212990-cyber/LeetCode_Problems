4bfd080c6f974e17110b55a6ead9540bfb5359a1
iVaanz <2k22.cse.2212990@gmail.com>
2025-11-19 15:00:17 +0530

    Time: 5 ms (58.63%) | Memory: 46.1 MB (87.16%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSub(String s) {
+        long res = 0;
+        long count = 0;
+        int mod = 1000000007;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                count++;
+                res = (res + count) % mod; 
+            } else {
+                count = 0;
+            }
+        }
+
+        return (int) res;
+    }
+}
